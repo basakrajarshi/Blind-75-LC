2541caa5dd826a442b370c4a9c92276763e46239
Rajarshi Basak <rajarshi.basak@colorado.edu>
2022-11-18 15:57:17 +0530

    Create 152. Maximum Product Subarray.py

---
+++ Array/152. Maximum Product Subarray.py
@@ -0,0 +1,18 @@
+def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = nums[0]
+        
+        for i in range(1, len(nums)):
+            temp_max = max(nums[i], max_so_far*nums[i], min_so_far*nums[i])
+            min_so_far = min(nums[i], max_so_far*nums[i], min_so_far*nums[i])
+            
+            max_so_far = temp_max
+            
+            result = max(result, max_so_far)
+            
+        return result
